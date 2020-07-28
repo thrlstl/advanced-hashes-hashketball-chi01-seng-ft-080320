82a3266f2e4b0890a9e9107500cb26dc8f37f815
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-28 04:17:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,3 +127,25 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored
+end
+
+def shoe_size
+end
+
+def team_colors
+end
+
+def team_names
+end
+
+def player_numbers
+end
+
+def player_stats
+end
+
+def big_shoe_rebounds
+end
+  
